6a6dee0a50b9649bd07f646f91d99fbf16e18d61
João D'Amorim <joao.damorim@gmail.com>
2023-02-02 09:16:06 -0300

    Creating about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+import styles from '../styles/About.module.css'
+
+export default function About() {
+    return (
+        <div className={styles.about}>
+            <h1>Sobre o projeto</h1>
+            <p>Projeto criado no curso de Next.js!</p>
+            <Image 
+                src="/images/charizard.png" 
+                width={300} 
+                height={300}
+                alt="Charizard" />
+        </div>
+    )
+}
