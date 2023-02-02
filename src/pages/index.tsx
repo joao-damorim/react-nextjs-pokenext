98e8b63b2ad1c26ac3e9d8f1dc0127e44bbfa8d6
João D'Amorim <joao.damorim@gmail.com>
2023-02-02 09:36:13 -0300

    Calling pokemons through the API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
 import styles from '@/styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemons = 150
+  const api = 'http://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  // add pokemon index
+  data.results.forEach((item: any, index: number) => {
+    item.id = index + 1
+  })
+  
+  return {
+    props: {
+      pokemons: data.results,
+    }
+  }
+}
+
+export default function Home({ pokemons }: any) {
   return (
-    <h1>PokeNext</h1>
+    <div>
+      <h1>PokeNext</h1>
+      <ul>
+      {pokemons.map((pokemon: any) => (
+            <li key={pokemon.id}>{pokemon.name}</li>
+          ))}
+      </ul>
+    </div>
   )
 }
